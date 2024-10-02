5a6fca37bfabf15008abd43f7bf0d68dcf4d5cfd
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-10-02 13:25:41 -0300

    fix: change choices array for Sex class import

--- pets/serializers.py
+++ pets/serializers.py
@@ -1,17 +1,16 @@
 from rest_framework import serializers
 from groups.serializers import GroupSerializer
 from traits.serializers import TraitSerializer
+from .models import Sex
 
 
 class PetSerializer(serializers.Serializer):
-
-    SEX_CHOICES = [("Male"), ("Female"), ("Not Informed")]
 
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
     age = serializers.IntegerField()
     weight = serializers.FloatField()
-    sex = serializers.ChoiceField(choices=SEX_CHOICES, default="Not Informed")
+    sex = serializers.ChoiceField(choices=Sex.choices, default=Sex.DEFAULT)
 
     group = GroupSerializer()
     traits = TraitSerializer(many=True)
